dcf5b33ec36da51eca1b131eacce182f832406db
Aime Fournier <aime.fournier@ucdenver.edu>
2016-10-14 12:53:57 -0600

    From Adam's 10/13 e-mail

--- aimefournier/matlab/Mesowest_downloader.m
+++ aimefournier/matlab/Mesowest_downloader.m
@@ -10,9 +10,13 @@ startdate =199901010000    %start time for data request YYYYMMDDHHMM
 enddate   =201609140000      %end time for data request  YYYYMMDDHHMM
 for stid = {'KCWV','KLHW','FSHU1','TT084','QLBA3', 'LCSS1'} % list of station ids to be downloaded
     % construct url for Mesowest API
-    url=sprintf('http://api.mesowest.net/v2/stations/timeseries?stid=%s&start=%d&end=%d&token=%s&output=csv&obtimezone=UTC',stid{:},startdate,enddate,token)
+    url=sprintf('http://api.mesowest.net/v2/stations/timeseries?stid=%s&start=%d&end=%d&token=%s&output=csv&obtimezone=UTC', ...
+       stid{:},startdate,enddate,token);
     % construct name of the csv file as station_id_STARTDATE-ENDDATE.csv'
-    fname=strcat(stid{:},'_',num2str(startdate),'-',num2str(enddate),'.csv')
+    fname=strcat(stid{:},'_',num2str(startdate),'-',num2str(enddate),'.csv');
+    tic
+    fprintf('Getting %s takes ', fname)
     % get the content of 'url' and save as 'fname'
     websave(fname,url)
+    fprintf('%6.3fs\n', toc)
 end
