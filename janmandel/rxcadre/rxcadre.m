f919e83efe79c6f267b0c52a1e36d74ff75e9d52
Jan Mandel <jan.mandel@gmail.com>
2016-10-17 10:01:10 -0600

    running rxcadre and replading tign in wrfout_d04

--- janmandel/rxcadre/rxcadre.m
+++ janmandel/rxcadre/rxcadre.m
@@ -1,20 +1,14 @@
-k=[kml2struct('../../KML/L2F_Fire1.kml'),kml2struct('../../KML/L2F_Fire2.kml')];save k.mat k
+%k=[kml2struct('../../KML/L2F_Fire1.kml'),kml2struct('../../KML/L2F_Fire2.kml')];save k.mat k
 load k.mat
 
-%dom=load_domain('wrfout_d04_2016-10-17_00:00:00');save dom.mat dom
-load dom.mat
+dom=load_domain('rxcadre-wrfxpy/wrf/wrfout_d04_2012-11-11_15:00:00');save dom.mat dom
+%load dom.mat
 
 ign=kml2ign(k,dom)
 
-start_times=char(dom.times')
-start_datenum=datenum(start_times);
-rxcadre_datenum=datenum(2012,11,11);
-for i=1:length(ign)
-    h=str2num(ign(i).Name(1:2));
-    m=str2num(ign(i).Name(3:4));
-    s=str2num(ign(i).Name(5:5));
-    % time in s since sim start
-    ign(i).t=(rxcadre_datenum-start_datenum)*12*3600+h*3600+m*60+s;
-end
+ign=rxcadre_update_ign(ign,dom)
+
 tign=make_tign(ign,dom);
 ncreplace('wrfinput_d04','TIGN_G',tign)
+save tign.mat tign
+fprintf('Minimal ignition time %gs from simulation start\n',min(tign(:)))
---
+++ janmandel/rxcadre/rxcadre_update_ign.m
@@ -0,0 +1,18 @@
+function ign_out=rxcadre_update_ignition(ign,dom)
+start_times=char(dom.times');
+start_datenum=datenum(start_times);
+rxcadre_datenum=datenum(2012,11,11);
+fprintf('Simulation start %s\n',datestr(start_datenum));
+fprintf('Ignition date    %s\n',datestr(rxcadre_datenum));
+ign_out=ign;
+for i=1:length(ign)
+    h=str2num(ign(i).Name(1:2));
+    m=str2num(ign(i).Name(3:4));
+    s=str2num(ign(i).Name(5:6));
+    % time in s since sim start
+    ign_out(i).datenum= rxcadre_datenum + (h*3600+m*60+s)/(24*3600);
+    ign_out(i).t=(rxcadre_datenum-start_datenum)*24*3600+h*3600+m*60+s;
+    fprintf('Ignition point %5d %s %s from start%6ds Lat%10.5f Lon%10.5f\n',...
+         i,ign_out(i).Name, datestr(ign_out(i).datenum),ign_out(i).t,ign_out(i).Lat,ign_out(i).Lon)
+end
+
