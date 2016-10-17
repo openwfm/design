036b80b42ff6e6e2291d52872ec13b011b5bcfb8
Jan Mandel <jan.mandel@gmail.com>
2016-10-17 00:15:28 -0600

    load starting time

--- janmandel/utils/load_domain.m
+++ janmandel/utils/load_domain.m
@@ -1,2 +1,2 @@
 function dom=load_domain(wrfout)
-dom=nc2struct(wrfout,{'FXLONG','FXLAT','UNIT_FXLONG','UNIT_FXLAT'},{},1);
+dom=nc2struct(wrfout,{'FXLONG','FXLAT','UNIT_FXLONG','UNIT_FXLAT','Times'},{},1);
