6b8a65f88e7e2d86bc6c10d249172b8555a24b4d
Jan Mandel <jan.mandel@gmail.com>
2016-10-16 18:51:02 -0600

    prints, fixing lowercase fiels

--- janmandel/utils/load_wrfout.m
+++ janmandel/utils/load_wrfout.m
@@ -1,17 +1,33 @@
-function dom=load_wrfout(base,search)
+function dom=load_wrfout(search,base)
+% dom=load_wrfout(search)
+% dom=load_wrfout(search,base)
+if ~exist('base','var')
+    base='.';
+end
 f = dir(fullfile(base,search));
 nfiles = length(f);
+if ~nfiles>0,
+    error('No files found')
+end
+vars={'LFN','FGRNHFX','TIGN_G','FIRE_AREA'};
 % get numbers of timesteps
+tsteps=zeros(1,nfiles);
 for i=1:nfiles
     file{i}=fullfile(base,f(i).name);
     p=ncdump(file{i},'-q');
     for j=1:length(p)
-        if strcmp(p(j).varname,'TIGN_G')
+        if strcmp(p(j).varname,vars{1})
              tsteps(i)=p(j).dimlength(3);
         end
     end
+    if tsteps(i)==0,
+        error(['Variable ',vars{1},' not found.'])
+    end
 end
+fprintf('Times steps: '),disp(tsteps)
+fprintf('Cumulative: '),disp(cumsum(tsteps))
 tstart=[1,1+cumsum(tsteps)];
+
 dom=load_domain(file{nfiles});
 p = nc2struct(file{nfiles},{'FIRE_AREA'},{},tsteps(nfiles));
 [isize,jsize]=size(p.fire_area);
@@ -21,9 +37,9 @@ iend   = min(isize,max(i)+5);
 jstart = max(1,min(j)-5);
 jend   = min(jsize,max(j)+5);
 fprintf('Interested in indices [ %d : %d ] x [%d: %d]\n',istart,iend,jstart,jend)
-vars={'LFN','FGRNHFX','TIGN_G','FIRE_AREA'};
 for j=1:length(vars),
-    s.(vars{j})=zeros(iend-istart+1,jend-jstart+1,tstart(nfiles+1)-1);
+    lvars{j}=lower(vars{j});
+    s.(lvars{j})=zeros(iend-istart+1,jend-jstart+1,tstart(nfiles+1)-1);
 end    
 for i=1:nfiles
     start=[istart-1,jstart-1,0];
@@ -31,7 +47,7 @@ for i=1:nfiles
     for j=1:length(vars),
         fprintf('Reading slice of %s from file %d %s\n',vars{j},i,file{i})
         p=ncvar(file{i},vars{j},start,count);
-        s.(vars{j})(:,:,tstart(i):tstart(i+1)-1)=p.var_value;
+        s.(lvars{j})(:,:,tstart(i):tstart(i+1)-1)=p.var_value;
     end
 end
 dom.sub=s;
