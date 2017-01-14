b030d97494d657ff37d714e7a8e42360acf99c32
Jan Mandel <jan.mandel@gmail.com`>
2017-01-14 11:14:16 -0700

    xls ignition

--- janmandel/ignition/make_tign.m
+++ janmandel/ignition/make_tign.m
@@ -12,9 +12,31 @@ function tign=make_tign(ign,dom,tign_max)
 %       x,y    coordinates of ignition point
 %       t      time of the ignition point
 %       r      radius where ignition will be enforced
+%
+% usage:
+% dom=load_domain(wrfout);
+% then
+%   % k=kml2struct(kml);ign = kml2ign(k,dom);
+% or
+%   ign = xls2ign(xls)
+% tign=make_tign(ign,dom);
+% ncreplace(wrfinput,tign);
+
+
+for k=1:length(ign)
+    lon(k)=ign(k).Lon;
+    lat(k)=ign(k).Lat;
+    t(k)=ign(k).t;
+end
+
+fprintf('ignition lon %g : %g\n',min(lon),max(lon));
+fprintf('ignition lat %g : %g\n',min(lat),max(lat));
+fprintf('domain   lon %g : %g\n',min(dom.fxlong(:)),max(dom.fxlong(:)));
+fprintf('domain   lat %g : %g\n',min(dom.fxlat(:)),max(dom.fxlat(:)));
+fprintf('time         %g : %g\n',min(t),max(t));
 
 % max pessimistic ignition time
-
+% 
 [nx,ny]=size(dom.fxlong);
 if ~exist('tign_max'),
     tign_max=max(nx*dom.unit_fxlong,ny*dom.unit_fxlat)/min(cell2mat(({ign.ros})));
@@ -29,7 +51,7 @@ for k=1:length(ign)
     % fire arrival time from ignition assuming given ros 
     tign(s)=min(tign(s),ign(k).t+d(s)/ign(k).ros);
 end 
-b=1000;
+b=1000+max(t);
 [i,j,v]=find(tign<b);
 mesh(min(b,tign(min(i):max(i),min(j):max(j))))
 end
