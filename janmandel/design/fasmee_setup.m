095f9cf9f14cfaf17e68ea1b0a5a617d7e8f2c95
Jan Mandel <jan.mandel@gmail.com>
2017-05-01 18:32:50 -0600

    fasmee_setup design on yellowstone

--- janmandel/design/fasmee_setup.m
+++ janmandel/design/fasmee_setup.m
@@ -1,3 +1,7 @@
+root_dir='/glade/u/home/jmandel/scratch/WRF341F_jm2_devel/wrffire/wrfv2_fire/test'
+template_dir=[root,'/Fishlake_template']
+wksp_dir = '/glade/u/home/jmandel/Projects/wrfxpy/wksp'
+
 generate=0
 if generate 
     N = 5;
@@ -8,34 +12,40 @@ if generate
                0.1,...           % probability value outside given interval
                N)                % sample points
     [X,P]=rLHS(D,1)
+
+save rep P X D N
     
 end
 
-save rep P X D N
 
 load rep
 for i = 1:N,
 
-    case_id=num2str(P(:,i))'
+    case_id=num2str(P(:,i))';
     job_id = ['Fishlake_',case_id]
     
-    nml = fileread('namelist.input.template');
+    job_dir = [wksp_dir,'/',job_id]
+    wrf_dir = [root_dir,'/',job_id]  % where wrf will run
+
+    shell(['mkdir ',job_dir])
+    shell(['cp -a ',template_dir,' ',wrf_dir])
+    shell(['ln -s ',job_dir,'/wrf ',wrf_dir])
+
+    nml = fileread([wrf_dir,'/namelist.input.template']);
     nml = strrep(nml,'_fire_ext_grnd_',num2str(D(2,i)));
     nml = strrep(nml,'_fire_atm_feedback_',num2str(D(3,i)));
-    nml_f =['namelist.input_',case_id];
-    filewrite(nml_f,nml)
+    filewrite([wrf_dir,'/namelist.input'],nml)
     
-    lsf = fileread('runwrf1.lsf.template');
+    lsf = fileread([wrf_dir,'/runwrf1.lsf.template']);
     lsf = strrep(lsf,'_job_id_',job_id);
-    filewrite('runwrf1.lsf',lsf)
+    filewrite([wrf_dir,'/runwrf1.lsf'],lsf)
     
-    job = fileread('job.json.template');
-    job = strrep(lsf,'_job_id_',job_id);
-    filewrite('job.json',job)
+    job = fileread([wrf_dir,'/job.json.template']);
+    job = strrep(job,'_job_id_',job_id);
+    filewrite([job_dir,'/job.json'],job)
     
     
-    wrf_f = ['wrfinput_d05_',case_id];
-    system(['/bin/cp -f wrfinput_d05.template ',wrf_f]);
+    wrf_f = [wrf_dir,'/wrfinput_d05'];
     fmc_gc=ncread(wrf_f,'FMC_GC');
     fmc_gc(:,:,1)=D(1,i)-0.01;
     fmc_gc(:,:,2)=D(1,i);
---
+++ janmandel/utils/shell.m
@@ -0,0 +1,4 @@
+function shell(s)
+disp(s)
+system(s)
+end
