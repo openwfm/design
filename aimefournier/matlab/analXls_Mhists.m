e72a2486df359270c12f3a14e6921926db9d6c1b
Aime Fournier <aime.fournier@ucdenver.edu>
2017-05-02 13:21:54 -0600

    abandoned

--- aimefournier/matlab/analXls_Mhists.m
+++ aimefournier/matlab/analXls_Mhists.m
@@ -1,12 +1,15 @@
 
 % Author: Aime' Fournier
 
- figure(2*sum(cell2mat({sta.nWin})) + 2)
+ cm = gray(max(arrayfun(@(x)x.nWin, sta)) + 2);
+ cm(end,:) = [];			% don't use white color
+ ib = @(a,x) a(1) <= x & x <= a(2);	% in-between condition
+ figure(lSta(end) + sum(cell2mat({sta.nWin})) + 1)
  clf
  n = 0;
  for i = lSta				% station loop:
-    yr = datevec(min(sta(i).t)) : datevec(max(sta(i).t));
-    nYr = length(yr);
+    yr = datevec(min(sta(i).t)) : ...
+         datevec(max(sta(i).t));
     cm = colormap(jet(nYr));		% color for each year
     for l = 1 : sta(i).nWin		% station-i window loop
        n = n + 1;
@@ -15,7 +18,7 @@
 	  text(.1, .5, sprintf('%s-%d has <%d burn days.', sta(i).name, l, max(sta(i).mom(l).n) + 1), 'FontSize', 20)
 	  axis off
        else
-	  for j = 1 : nYr		% year loop at station i:
+	  for j = 1 : length(yr)	% year loop at station i:
 	     %
 	     % LGST can be -Inf:
 	     %
