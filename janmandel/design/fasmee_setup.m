9802de4526acf4aa362d0de026a4370fec8212d0
Jan Mandel <jan.mandel@gmail.com>
2017-05-05 00:14:54 -0600

    LSH3

--- janmandel/design/fasmee_setup.m
+++ janmandel/design/fasmee_setup.m
@@ -7,12 +7,6 @@ clone=1
 
 if generate 
     N = 5;
-    [D,logm,logs]=equal_logn(...
-              [0.04 0.14         % 10 h moisture
-               6,     50         % heat ext depth         fire_ext_grnd
-               0.5     2],...     % heat flux multiplier  fire_atm_feedback
-               0.1,...           % probability value outside given interval
-               N)                % sample points
     [X,P]=rLHS(D,1)
 
 save rep1 P X D N
@@ -22,11 +16,36 @@ end
 
 if clone
 load rep1
+
+[D,logm,logs]=equal_logn(...
+              [0.05 0.14         % 10 h moisture
+               6,     50         % heat ext depth         fire_ext_grnd
+               0.5    2],...  % heat flux multiplier  fire_atm_feedback
+               0.1,...           % probability value outside given interval
+               N)                % sample points
+
+%P =[1;1;5]
+
+[L,N,r]=size(P);
+
+for k=1:r
+    for j=1:L
+        pp=P(j,:,k);
+        X(j,:,k)=D(j,pp);
+    end
+end
+
 for i = 1:N,
 
     case_id=num2str(P(:,i))';
-    job_id = ['LHS_Fishlake_',case_id]
-    
+    job_id = ['LSH3_Fishlake_',case_id];
+    fmc_gc_10h = X(1,i);
+    fire_ext_grnd = X(2,i);
+    fire_atm_feedback=X(3,i);
+    description = sprintf('%s fmc_gc_10h=%g fire_ext_grnd=%g fire_atm_feedback=%g',...
+        job_id,fmc_gc_10h,fire_ext_grnd,fire_atm_feedback);
+    disp(description)
+
     job_dir = [wksp_dir,'/',job_id]
     wrf_dir = [root_dir,'/',job_id]  % where wrf will run
 
@@ -37,9 +56,7 @@ for i = 1:N,
     shell(['ln -s ',wrf_dir,' ',job_dir,'/wrf '])
 
     nml = fileread([wrf_dir,'/namelist.input.template']);
-    fire_ext_grnd = X(2,i)
     nml = strrep(nml,'_fire_ext_grnd_',num2str(fire_ext_grnd));
-    fire_atm_feedback=X(3,i)
     nml = strrep(nml,'_fire_atm_feedback_',num2str(fire_atm_feedback));
     filewrite([wrf_dir,'/namelist.input'],nml)
     
@@ -48,16 +65,15 @@ for i = 1:N,
     filewrite([wrf_dir,'/runwrf1.lsf'],lsf)
     
     job = fileread([wrf_dir,'/job.json.template']);
+    job = strrep(job,'_description_',description);
     job = strrep(job,'_job_id_',job_id);
     filewrite([job_dir,'/job.json'],job)
     
-    
     wrf_f = [wrf_dir,'/wrfinput_d05'];
-    fmc10h = X(1,i)
     fmc_gc=ncread(wrf_f,'FMC_GC');
-    fmc_gc(:,:,1)=fmc10h-0.01;
-    fmc_gc(:,:,2)=fmc10h;
-    fmc_gc(:,:,3)=fmc10h+0.01;
+    fmc_gc(:,:,1)=fmc_gc_10h-0.01;
+    fmc_gc(:,:,2)=fmc_gc_10h;
+    fmc_gc(:,:,3)=fmc_gc_10h+0.01;
     fmc_gc(:,:,4)=0.05;
     fmc_gc(:,:,5)=0.78;
     ncreplace(wrf_f,'FMC_GC',fmc_gc);
