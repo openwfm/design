97b664f52f4f69272fd103cff18c1060c5b4ac22
Jan Mandel <jan.mandel@gmail.com`>
2017-05-11 08:47:09 -0600

    fix

--- janmandel/design/interpw2height.m
+++ janmandel/design/interpw2height.m
@@ -6,7 +6,7 @@ for k=1:size(alt_at_w,3)    % convert into height above the terrain
     hgt_at_w(:,:,k,:)=alt_at_w(:,:,k,:)-alt_at_w(:,:,1,:);
 end
 if strcmp(var,'w')
-    hgt=hgt_at_w(:,:,:,:);
+    hgt=hgt_at_w;
     fprintf('interpolating staggered variable %s to height %im\n',var,h)
 else  % assuming given in center
     hgt=0.5*(hgt_at_w(:,:,2:end,:)+hgt_at_w(:,:,1:end-1,:));
@@ -21,14 +21,14 @@ for it=1:t
         for im=1:m
             %x=squeeze(hgt_at_w(im,in,:,it));
             %y=squeeze(p.w(im,in,:,it));
-            for i=2:kk
-                x1=hgt(im,in,i,it);
-                if h<x1
+            for k=2:kk
+                x1=hgt(im,in,k,it);
+                if h>x1
                 else
-                    y1=var(im,in,i,it);
-                    y0=var(im,in,i-1,it);
-                    x0=hgt(im,in,i-1,it);
-                    var_at_h(im,in,it)=y0+(y1-y0)*(h-x0)/(x1-x0);
+                    y1=var(im,in,k,it);
+                    y0=var(im,in,k-1,it);
+                    x0=hgt(im,in,k-1,it);
+                    var_at_h(im,in,it)=(y0*(x1-h)+y1*(h-x0))/(x1-x0);
                     break
                 end
             end
